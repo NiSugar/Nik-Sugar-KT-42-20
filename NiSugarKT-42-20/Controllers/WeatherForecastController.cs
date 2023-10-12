db5999089a9e3c3f5a18d0af7fc76d4eca5d889a
kolua <kolua@DESKTOP-9BRB65T>
2023-10-12 15:29:26 +0300

    add logging and WeatherForecast test request by Saharov

--- NiSugarKT-42-20/Controllers/WeatherForecastController.cs
+++ NiSugarKT-42-20/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace NiSugarKT_42_20.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -29,5 +31,16 @@ namespace NiSugarKT_42_20.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
+        }
+
     }
 }
