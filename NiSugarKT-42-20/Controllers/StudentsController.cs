d5279bd5b751051a96fec605d8e2310015f3c101
kolua <kolua@DESKTOP-9BRB65T>
2024-01-11 00:12:06 +0300

    Add ADDing, EDITing and DELETing for Students (+LB4)

--- NiSugarKT-42-20/Controllers/StudentsController.cs
+++ NiSugarKT-42-20/Controllers/StudentsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using NiSugarKT_42_20.Interfaces.StudentInterfaces;
 using NiSugarKT_42_20.Database;
 using NiSugarKT_42_20.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace NiSugarKT_42_20.Controllers
 {
@@ -12,11 +13,14 @@ namespace NiSugarKT_42_20.Controllers
     {
         private readonly ILogger<StudentsController> _logger;
         private readonly IStudentService _studentService;
+        private StudentDbContext _context;
 
-        public StudentsController(ILogger<StudentsController> logger, IStudentService studentService)
+        public StudentsController(ILogger<StudentsController> logger, IStudentService studentService, StudentDbContext context)
         {
             _logger = logger;
             _studentService = studentService;
+            _context = context;
+
         }
 
         [HttpPost("GetStudentsByGroup")]
@@ -41,6 +45,66 @@ namespace NiSugarKT_42_20.Controllers
             var students = await _studentService.GetStudentsByFIOAsync(filter, cancellationToken);
 
             return Ok(students);
+        }
+
+        // ДОБАВЛЕНИЕ СТУДЕНТА
+        [HttpPost("AddStudent")]
+        public IActionResult CreateStudent([FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Add(student);
+            _context.SaveChanges();
+            return Ok(student);
+        }
+
+        // РЕДАКТИРОВАНИЕ СТУДЕНТА
+        [HttpPut("EditStudent")]
+        public IActionResult EditStudent(string LastName, [FromBody] Student editedStudent)
+        {
+            var existingStudent = _context.Students.FirstOrDefault(s => s.LastName == LastName);
+
+            if (existingStudent == null)
+            {
+                return NotFound();
+            }
+
+            existingStudent.FirstName = editedStudent.FirstName;
+            existingStudent.LastName = editedStudent.LastName;
+            existingStudent.MiddleName = editedStudent.MiddleName;
+            var editedStGroup = _context.Groups.FirstOrDefault(g => g.GroupId == editedStudent.GroupId);
+            if (editedStGroup == null)
+            {
+                existingStudent.GroupId = editedStudent.GroupId;
+            }
+            else
+            {
+                existingStudent.GroupId = editedStGroup.GroupId;
+            }
+
+            existingStudent.StudentExist = editedStudent.StudentExist;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // УДАЛЕНИЕ СТУДЕНТА
+        [HttpDelete("DeleteStudent")]
+        public IActionResult DeleteStudent(string LastName)
+        {
+            var existingStudent = _context.Students.FirstOrDefault(s => s.LastName == LastName);
+
+            if (existingStudent == null) { return NotFound(); }
+
+            //_context.Students.Remove(existingStudent);
+
+            existingStudent.StudentExist = false;
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
